35ea7f4379228e5b9ab90141b625b150d1472dde
itsrajaniket <itsrajaniket@gmail.com>
2025-11-07 18:51:51 +0530

    testing the multiple

---
+++ test.js
@@ -0,0 +1,30 @@
+
+//  console.log("Aniket");
+//  alert("aniket");
+// console.log("its my first code")
+// let name = prompt("enter your name" );
+// console.log (name);
+
+// let prime = prompt("enter your number to check its prime or not ")
+// console.log("your number is" , prime)
+// if ( prime % 5 === 0){
+//     console.log(prime,"is multiple of 5 ");
+// }
+// else{
+//     console.log(prime,"is NOT multiple of 5");
+// }
+
+
+// ifelse(prime == 0)
+// {
+// console.log("you enter a number that is zero ")
+// }
+
+
+let student = prompt("Enter the name of student")
+console.log("Student name is " , student )
+let score = prompt("enter student marks" )
+console.log( student , " marks is " , score )
+if (score => 80 , score >=100 ){
+    console.log("A")
+}
