86d6bc52470b3a988dc2e280b02addbb9df3734f
itsrajaniket <itsrajaniket@gmail.com>
2025-11-08 17:07:30 +0530

    add loop code

---
+++ loop.js
@@ -0,0 +1,36 @@
+
+// print text for 5 time 
+
+// let print = "aniket"
+// for(let i=0; i<=9; i++ ){
+// console.log(print)} 
+
+
+// sum of number till n 
+
+
+// let number = prompt("Enter a number :")
+// let sum = 0
+// for(let i = 1; i<=number ; i++ ){
+//     sum = sum + i
+// }
+// console.log("sum of all number till",number,"=" ,sum )
+
+
+
+// while loop
+
+
+// let i =1 ;
+// while(i<=10){
+//     i++;
+//     console.log("loop")
+// }
+
+
+//do while 
+let i = 1;
+let fun = i;
+do{console.log("fun");
+    i++;
+} while(i <= 5);
